a1efb92135be42e71293093960870f0279babf6b
kshitiz20 <kshitizsharma62@gmail.com>
2019-01-04 05:19:00 +0000

    Adding GET /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,16 @@ app.post('/todos',(req, res)=>{
 })
 
 
+app.get('/todos',(req, res)=>{
+    TodoModel.find({}).then((todos)=>{
+        console.log(todos);
+        res.send(todos);
+    }).catch(err=>{
+        res.status(400).send(err);
+        console.log(err);
+    })
+})
+
 
 
 module.exports={app};
